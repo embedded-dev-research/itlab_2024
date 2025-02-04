16e13d499cb4bcec8cc3134aa88d33a2c337badc
weyland0 <58119906+weyland0@users.noreply.github.com>
2025-02-04 21:59:10 +0300

    Fix main.cpp
    Indent was changed from 4 to 2 spaces

--- app/extract-network-data/main.cpp
+++ app/extract-network-data/main.cpp
@@ -1,21 +1,18 @@
 
 #include <iostream>
-#include <vector>
-#include <string>
-
 #include <opencv2/core.hpp>
 #include <opencv2/dnn/dnn.hpp>
+#include <string>
+#include <vector>
 
-int main()
-{
-    std::string modelPath = cv::samples::findFile("../yolov8s.onnx");
-    cv::dnn::Net net = cv::dnn::readNetFromONNX(modelPath);
-    std::vector<std::string> layerNames = net.getLayerNames();
+int main() {
+  std::string modelPath = cv::samples::findFile("yolov8s.onnx");
+  cv::dnn::Net net = cv::dnn::readNetFromONNX(modelPath);
+  std::vector<std::string> layerNames = net.getLayerNames();
 
-    for (auto& name : layerNames)
-    {
-        std::cout << name << std::endl;
-    }
+  for (auto& name : layerNames) {
+    std::cout << name << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
