7c87d3d8b04ab5c3000cbb0f6fe79246a343ab19
chekalexey <alexche2509@gmail.com>
2025-03-11 15:19:27 +0300

    Add a ElementwiseLayer Example

--- app/layer_example/ElementwiseLayer.cpp
+++ app/layer_example/ElementwiseLayer.cpp
@@ -1,31 +1,76 @@
-#include "arm_compute/runtime/NEON/functions/NEElementwiseOperations.h"
+#include "arm_compute/runtime/NEON/NEFunctions.h"
 #include "utils/Utils.h"
 
 #include <iostream>
 using namespace arm_compute;
 using namespace utils;
 
-int main() {
+class ElementwiseLayer {
     const int input_width = 5;
     const int input_height = 5;
 
     Tensor input1, input2, output;
 
-    input1.allocator()->init(TensorInfo(TensorShape(input_width, input_height, 1), 1, DataType::F32));
-    input2.allocator()->init(TensorInfo(TensorShape(input_width, input_height, 1), 1, DataType::F32));
-    output.allocator()->init(TensorInfo(TensorShape(input_width, input_height, 1), 1, DataType::F32));
+public:
+    void fill() {
+        input1.allocator()->init(TensorInfo(TensorShape(input_width, input_height, 1), 1, DataType::F32));
+        input2.allocator()->init(TensorInfo(TensorShape(input_width, input_height, 1), 1, DataType::F32));
+        output.allocator()->init(TensorInfo(TensorShape(input_width, input_height, 1), 1, DataType::F32));
 
-    input1.allocator()->allocate();
-    input2.allocator()->allocate();
-    output.allocator()->allocate();
+        input1.allocator()->allocate();
+        input2.allocator()->allocate();
+        output.allocator()->allocate();
 
-    fill_random_tensor(input1, 0.f, 1.f);
-    fill_random_tensor(input2, 0.f, 1.f);
+        fill_random_tensor(input1, 0.f, 1.f);
+        fill_random_tensor(input2, 0.f, 1.f);
+    }
 
-    NEElementwiseSquaredDiff elementwise;
-    elementwise.configure(&input1, &input2, &output);
+    void SquaredDiff() {
+        NEElementwiseSquaredDiff elementwise;
+        elementwise.configure(&input1, &input2, &output);
+        elementwise.run();
+    }
+    
+    void Division() {
+        NEElementwiseDivision elementwise;
+        elementwise.configure(&input1, &input2, &output);
+        elementwise.run();
+    }
+    
+    void Addition() {
+        NEArithmeticAddition add;
+        add.configure(&input1, &input2, &output);
+        add.run();
+    }
+    
+    void Swish() {
+        NEActivationLayer act;
+        act.configure(&input1, &input2, ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::SWISH));
+        act.run();
+    }
+    
+    void Abs() {
+        NEActivationLayer act;
+        act.configure(&input1, &input2, ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::ABS));
+        act.run();
+    }
 
-    elementwise.run();
+    void Sigmoid() {
+        NEActivationLayer act;
+        act.configure(&input1, &input2, ActivationLayerInfo(ActivationLayerInfo::ActivationFunction::LOGISTIC));
+        act.run();
+    }
 
-    output.print(std::cout);
+    void print() {
+        output.print(std::cout);
+    }
+};
+
+int main() {
+    ElementwiseLayer a;
+    a.fill();
+    a.Addition();
+    a.print();
+
+    return 0;
 }
