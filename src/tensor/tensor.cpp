9dcf39d1e30bfa7dfd53884e18866824b6a92730
Vlad <vaaoala3@gmail.com>
2025-03-25 12:35:31 +0300

    tensor built fix

--- src/tensor/tensor.cpp
+++ src/tensor/tensor.cpp
@@ -10,7 +10,7 @@
 #include <utility>
 
 Shape::Shape(std::vector<size_t> dims) : dimensions(std::move(dims)) {
-  total_elements = std::accumulate(dimensions.begin(), dimensions.end(), static_cast<size_t>(1), std::multiplies<>());
+  total_elements = std::accumulate(dimensions.begin(), dimensions.end(), static_cast<size_t>(1), [](size_t a, size_t b) { return a * b; });
 }
 
 size_t Shape::get_rank() const { return dimensions.size(); }
