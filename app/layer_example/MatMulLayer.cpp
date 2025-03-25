f02551d3b7f32c152f8974c842620f57c5739cd0
chekalexey <alexche2509@gmail.com>
2025-03-25 15:22:17 +0300

    add a MatmulLayer example

---
+++ app/layer_example/MatMulLayer.cpp
@@ -0,0 +1,32 @@
+#include "arm_compute/runtime/NEON/NEFunctions.h"
+#include "utils/Utils.h"
+
+#include <iostream>
+using namespace arm_compute;
+using namespace utils;
+
+int main() {
+    Tensor input1;
+    Tensor input2;
+    Tensor output;
+    
+    const int input_width  = 5;
+    const int input_height = 5;
+
+    input1.allocator()->init(TensorInfo(TensorShape(input_width, input_height, 1), 1, DataType::F32));
+    input2.allocator()->init(TensorInfo(TensorShape(input_width, input_height, 1), 1, DataType::F32));
+    output.allocator()->init(TensorInfo(TensorShape(input_width, input_height, 1), 1, DataType::F32));
+    
+    input1.allocator()->allocate();
+    input2.allocator()->allocate();
+    output.allocator()->allocate();
+    
+    fill_random_tensor(input1, 0.f, 1.f);
+    fill_random_tensor(input2, 0.f, 1.f);
+
+    NEMatMulLayer m; 
+    m.configure(&input1, &input2, &output);
+    m.run();
+
+    output.print(std::cout);
+}
