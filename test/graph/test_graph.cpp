e9c92bfa630f992bdffc23e01660066b4e78e454
Vlad <vaaoala3@gmail.com>
2025-04-08 00:10:39 +0300

    tests fix

--- test/graph/test_graph.cpp
+++ test/graph/test_graph.cpp
@@ -3,38 +3,38 @@
 #include "graph/graph.h"
 #include "gtest/gtest.h"
 
-TEST(Graph, can_create_graph) { ASSERT_NO_THROW(Graph g); }
-
 TEST(Graph, can_add_vertex_to_graph) {
   Graph g;
 
-  ASSERT_NO_THROW(g.addVertex(1));
+  g.addVertex(1);
+
+  ASSERT_EQ(1, g.getVertices());
 }
 
 TEST(Graph, can_add_edge_to_graph) {
   Graph g;
 
-  ASSERT_NO_THROW(g.addEdge(1, 0));
+  g.addEdge(1, 0);
+
+  ASSERT_EQ(1, g.getEdges());
 }
 
-TEST(Graph, can_add_edge_with_same_id_to_graph) {
+TEST(Graph, cant_add_edge_with_same_id_to_graph) {
   Graph g;
 
-  ASSERT_NO_THROW(g.addEdge(1, 1));
+  g.addEdge(1, 1);
+  
+  ASSERT_EQ(0, g.getEdges());
 }
 
 TEST(Graph, can_add_vertex_and_edge_to_graph) {
   Graph g;
 
-  ASSERT_NO_THROW(g.addEdge(1, 0));
-  ASSERT_NO_THROW(g.addVertex(1));
-}
-
-TEST(Graph, can_get_vertex) {
-  Graph g;
   g.addEdge(1, 0);
+  g.addVertex(1);
 
-  ASSERT_NO_THROW(g.getVertex());
+  ASSERT_EQ(1, g.getEdges());
+  ASSERT_EQ(1, g.getEdges());
 }
 
 TEST(Graph, can_remove_vertex) {
@@ -43,10 +43,10 @@ TEST(Graph, can_remove_vertex) {
 
   g.removeVertex(1);
 
-  ASSERT_EQ(g.vertexCount(), 1);
+  ASSERT_EQ(g.getVertices(), 1);
 }
 
-TEST(Graph, can_get_vertex_count) {
+TEST(Graph, can_get_vertices_count) {
   Graph g;
 
   g.addEdge(1, 0);
@@ -56,10 +56,10 @@ TEST(Graph, can_get_vertex_count) {
   g.addVertex(1);
   g.removeVertex(3);
 
-  ASSERT_EQ(g.vertexCount(), 3);
+  ASSERT_EQ(g.getVertices(), 3);
 }
 
-TEST(Graph, can_get_edge_count) {
+TEST(Graph, can_get_edges_count) {
   Graph g;
 
   g.addEdge(1, 0);
@@ -67,7 +67,7 @@ TEST(Graph, can_get_edge_count) {
   g.addEdge(0, 2);
   g.addEdge(2, 2);
 
-  ASSERT_EQ(g.edgeCount(), 3);
+  ASSERT_EQ(g.getEdges(), 3);
 }
 
 TEST(Graph, check_graph_is_empty) {
@@ -119,20 +119,11 @@ TEST(Graph, check_graph_can_find_path_between_vertexes_after_delete) {
   ASSERT_FALSE(g.hasPath(0, 3));
 }
 
-TEST(Graph, can_create_bfs_path) {
-  Graph g;
-
-  g.addEdge(0, 1);
-  g.addEdge(0, 3);
-  g.addEdge(1, 2);
-
-  ASSERT_NO_THROW(g.BFS(0));
-}
-
 TEST(Graph, can_create_bfs_path_in_empty_graph) {
   Graph g;
+  std::vector<int> v1 = {0};
 
-  ASSERT_NO_THROW(g.BFS(0));
+  ASSERT_EQ(g.BFS(0),v1);
 }
 
 TEST(Graph, check_bfs_path) {
