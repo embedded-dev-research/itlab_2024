97c7d81dcf2c5b5d2c676d25c27c71a10d3303e5
AlKurpiakov <s23380350@unn.ru>
2025-04-29 19:06:44 +0300

    Текущая ветка: ONNX-Model-Parser  Эта ветка соответствует «origin/ONNX-Model-Parser».  Изменения, которые будут включены в коммит: 	изменено:      app/frontend/CMakeLists.txt 	изменено:      app/frontend/GetModel.sh

--- app/frontend/GetModel.sh
+++ app/frontend/GetModel.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-protoc --proto_path=./3rdparty/onnx/onnx --cpp_out=$1 onnx.proto
+protoc --proto_path=3rdparty/onnx/onnx --cpp_out=$1 onnx.proto
 yolo export model=yolo11x.pt format=onnx save_dir=$1
-
-
