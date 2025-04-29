0ea25ea39a00d8d81f09bbd8ead5511fa2bc5a78
AlKurpiakov <s23380350@unn.ru>
2025-04-29 19:02:25 +0300

    Текущая ветка: ONNX-Model-Parser  Эта ветка соответствует «origin/ONNX-Model-Parser».
     Изменения, которые будут включены в коммит:
    	изменено:      app/frontend/GetModel.sh

--- app/frontend/GetModel.sh
+++ app/frontend/GetModel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-protoc --proto_path=../../3rdparty/onnx/onnx --cpp_out=./generated onnx.proto
-yolo export model=yolo11x.pt format=onnx save_dir=./generated
+protoc --proto_path=./3rdparty/onnx/onnx --cpp_out=$1 onnx.proto
+yolo export model=yolo11x.pt format=onnx save_dir=$1
 
 
